644b666c514444c458074f793fac665ab88a855e
MihaMixFromGB <mihamix@yandex.ru>
2022-12-28 18:05:34 +0700

    2022-12-28 18:05

---
+++ apps/api/api/vercel.ts
@@ -0,0 +1,43 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from '../src/app/app.module';
+import { UserEntity } from '../../../libs/entities/src';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  // const globalPrefix = 'api';
+  const globalPrefix = '';
+  app.setGlobalPrefix(globalPrefix);
+
+  app.use(cookieParser());
+  app.useStaticAssets(join(__dirname, 'assets'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    })
+  );
+
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
