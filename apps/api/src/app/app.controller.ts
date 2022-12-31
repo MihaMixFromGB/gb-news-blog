d8cf052b5d2a1d9b0d485372b2315258f6247d9f
MihaMixFromGB <mihamix@yandex.ru>
2022-12-31 12:34:51 +0700

    2022-12-31 12:34

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,9 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 
-// import { existsSync } from 'fs';
-import { readdirSync } from 'fs';
-import { join } from 'path';
-
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,23 +9,5 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
-  }
-
-  @Get('console')
-  getLog() {
-    let log = '';
-    log += '__dirname: ' + __dirname + ' --- ';
-    // log += 'assets: ' + join(__dirname, '..', 'assets');
-    // log += 'assets: ' + existsSync(join(__dirname, '..', 'assets')) + ' --- ';
-    // log +=
-    //   'batman.jpg: ' +
-    //   existsSync(join(__dirname, '..', 'assets/images/batman.jpg'));
-
-    const testFolder = join(__dirname, '..');
-    readdirSync(testFolder).forEach((file) => {
-      log += file + ' --- ';
-    });
-
-    return log;
   }
 }
